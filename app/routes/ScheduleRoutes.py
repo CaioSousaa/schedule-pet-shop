27374d3bb54431a89061e9ee55c8c731e7a556f0
caio <caiothalles607@gmail.com>
2024-11-20 21:33:15 -0300

    feat: created service for get all schedules

--- app/routes/ScheduleRoutes.py
+++ app/routes/ScheduleRoutes.py
@@ -1,14 +1,26 @@
+from app.usecases.schedule_usecases.GetAllSchedules import get_all_schedule
 from app.usecases.schedule_usecases.CreateSchedule import create_schedule
 from fastapi import APIRouter, HTTPException
 from datetime import date
 from uuid import UUID
 
+
 router = APIRouter()
 
-@router.post("/schedule/")
+@router.post("/schedule")
 async def create_schedule_routes(service_id: int, client_id: UUID, date_schedule: date):
     try:
         create_schedule(service_id=service_id, client_id=client_id, date_schedule=date_schedule)
         return {"msg": "Agendamento criado com sucesso"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao criar o agendamento: {str(e)}")
+
+@router.get("/schedule/all")
+async def get_all():
+    try:
+        schedules = get_all_schedule()
+        if not schedules:
+            return {"msg":"nenhum agendamento  registrado"}
+        return schedules
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao obter os agendamentos: {str(e)}")
---
+++ app/usecases/schedule_usecases/GetAllSchedules.py
@@ -0,0 +1,26 @@
+import os 
+
+FILE = "csv/schedule_csv.csv"
+
+def get_all_schedule():
+    if not os.path.exists(FILE):
+        print("Nenhum agendamento marcado")
+        return
+    
+    schedules = []
+
+    with open(FILE, "r") as file:
+        header = next(file, None)
+        if header is None:
+            print("O arquivo esta vazio")
+            return []
+        
+        for line in file:
+            line = line.strip()
+            if line:
+                id, id_client, id_sevice, date_schedule = line.split(",")
+                schedules.append(
+                    {"id":id, "id_client": id_client, "id_service":id_sevice, "date_schedule": date_schedule}
+                
+                )
+    return schedules
